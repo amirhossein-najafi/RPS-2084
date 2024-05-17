7f2ec0f6635110e8d979afcb81d00bc71a725d73
amirhossein-najafi <njamir16@gmail.com>
2024-05-17 22:07:46 +0330

    edit code

--- game2player/main.js
+++ game2player/main.js
@@ -12,8 +12,8 @@ function playGame(){
     const user2score = document.querySelector('.score2');
     const action1 = document.getElementById('action1');
     const action2 = document.getElementById('action2');
-    const result1 = document.getElementsByI('result1');
-    const result2 = document.getElementsByI('result2');
+    const result1 = document.getElementById('result1');
+    const result2 = document.getElementById('result2');
     let userAction1, userAction2;
     let userScore1 = 0;
     let userScore2 = 0;
@@ -24,11 +24,11 @@ function playGame(){
     }
     document.addEventListener('keydown', function(event) {
         const key = event.key.toLowerCase();
-        if (key === 'a') {
+        if (key === 'r') {
             userAction1 = 'rock';
-        } else if (key === 's') {
+        } else if (key === 'p') {
             userAction1 = 'paper';
-        } else if (key === 'd') {
+        } else if (key === 's') {
             userAction1 = 'scissors';
         }
         game(userAction1, userAction2);
@@ -38,9 +38,9 @@ function playGame(){
         const key = event.key;
         if (key === 'ArrowLeft') {
             userAction2 = 'rock';
-        } else if (key === 'ArrowUp') {
-            userAction2 = 'paper';
         } else if (key === 'ArrowRight') {
+            userAction2 = 'paper';
+        } else if (key === 'ArrowUp') {
             userAction2 = 'scissors';
         }
         game(userAction1, userAction2);
@@ -56,8 +56,8 @@ function playGame(){
             action2.innerText = 'You Lost!';
             action1.style.color = "green";
             action2.style.color = "red";
-            result1.innerText = "choose "+userAction1;
-            result2.innerText = "choose "+userAction2;
+            result1.innerText = "user 1 choose "+userAction1;
+            result2.innerText = "user 2 choose "+userAction2;
             userScore2++;
             user1score.textContent = userScore2.toString();
         } else if (
@@ -67,19 +67,15 @@ function playGame(){
         ) {
             action2.innerText = 'You Won!';
             action1.innerText = 'You Lost!';
-            action2.style.color = "green";
-            action1.style.color = "red";
-            result1.innerText = " user 1 choosed "+userAction1;
-            result2.innerText = "user 2 chossed "+userAction2;
+            result1.innerText = " user 1 choose "+userAction1;
+            result2.innerText = "user 2 choose "+userAction2;
             userScore1++;
             user2score.textContent = userScore1.toString();
         } else{
             action2.innerText = 'Tie!';
             action1.innerText = 'Tie!';
-            action2.style.color = "black";
-            action1.style.color = "black";
-            result1.innerText = "user 1 choosed "+userAction1;
-            result2.innerText = "user 2 chossed "+userAction2;
+            result1.innerText = "user 1 choose "+userAction1;
+            result2.innerText = "user 2 choose "+userAction2;
         }
         userAction1 = null;
         userAction2 = null;
